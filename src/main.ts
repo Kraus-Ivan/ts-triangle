470c5ca181ac89e230b102bfbbd40a8fe9e099ca
Ivan Kraus <ivan.kraus@post.cz>
2023-09-18 15:51:08 +0200

    going insane

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,46 @@
-import { Triangle } from "./triangle.ts";  
-
-let triangle = new Triangle(3, 4, 5);
-
-console.log(triangle.CoordB);
+import { Triangle } from "./triangle.ts";
 
 
+let initialTriangle = new Triangle(3, 4, 5);
+console.log(initialTriangle.CoordB); 
 
-const form = document.getElementById('triangleForm');
+let form = document.getElementById('triangleForm');
 
-const sideA = document.getElementById('sideA');
-const sideB = document.getElementById('sideB');
-const sideC = document.getElementById('sideC');
+let sideA = document.getElementById('sideA') as HTMLInputElement;
+let sideB = document.getElementById('sideB') as HTMLInputElement;
+let sideC = document.getElementById('sideC') as HTMLInputElement;
 
-const alpha = document.getElementById('alpha');
-const beta = document.getElementById('beta');
-const gamma = document.getElementById('gamma');
+let alpha = document.getElementById('alpha') as HTMLInputElement;
+let beta = document.getElementById('beta') as HTMLInputElement;
+let gamma = document.getElementById('gamma') as HTMLInputElement;
 
-const perimeter = document.getElementById('perimeter');
-const content = document.getElementById('content');
+let perimeter = document.getElementById('perimeter') as HTMLInputElement;
+let content = document.getElementById('content') as HTMLInputElement;
 
-const resultFeedback = document.getElementById('result-feedback');
+let resultFeedback = document.getElementById('result-feedback') as HTMLElement;
 
-const lineAB = document.getElementById('lineAB');
+if (form && sideA && sideB && sideC && alpha && beta && gamma && perimeter && content && resultFeedback) {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let sA: number = parseFloat(sideA.value);
+        let sB: number = parseFloat(sideB.value);
+        let sC: number = parseFloat(sideC.value);
+        let triangle = new Triangle(sA, sB, sC);
+
+        if(triangle.Constructable) {
+            alpha.value = String(triangle.Alpha);
+            beta.value = String(triangle.Beta);
+            gamma.value = String(triangle.Gamma);
+    
+            perimeter.value = String(triangle.Perimeter);
+            content.value = String(triangle.Content);
+            
+        }
+
+        else {
+            console.log("Trojúhelník nelze sestrojit.")
+        }
+
+    });
+}
